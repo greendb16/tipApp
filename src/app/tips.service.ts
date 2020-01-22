77dce322075e7963b39bb15c880a0f5b3e8f4e4f
greendb16 <greendb16@gmail.com>
2020-01-22 11:12:35 -0500

    Final Wed Commit

--- src/app/tips.service.ts
+++ src/app/tips.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-// import data from './tips.json' 
+// import {myTips} from Tips.json; 
 
 @Injectable({
   providedIn: 'root'
--- src/app/www/js/app.js
+++ src/app/www/js/app.js
@@ -4,6 +4,7 @@ angular.module('tipMod',['ionic'])
 
 .controller('tipCtrl', function($scope, $http){
     
+    // Function to fetch tips
     $http.get(app.js)
     .then(function(response){
         var rand=Math.floor(Math.random() * 10);
@@ -11,3 +12,14 @@ angular.module('tipMod',['ionic'])
         $scope.cTip=currentTip
     })
 })
+
+// Function to change background color
+angular.module('bG', ['ionic'])
+        .controller('backgroundCtrl',($scope), function($scope){
+                // var rand=Math.floor(Math.random() * 2);
+                  var rand =1;
+                  if(rand==1){
+                    $scope.randBackG = ".bG1";
+                  }
+              }
+        )
